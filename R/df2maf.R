dac7f1488045d9e76d6834d58e204921ff9e14ba
selkamand <sam.elkamand@gmail.com>
2023-11-24 13:24:32 +1100

    fix: dropped broken example

--- R/df2maf.R
+++ R/df2maf.R
@@ -33,11 +33,6 @@
 #' @return a maf-like data.frame (data.table)
 #' @export
 #'
-#' @examples
-#' path_mutations <- system.file("testfiles/pedcbioportal_mutation_annotated.tsv", package = "vcf2mafR")
-#' if (!file.exists(path_mutations)) stop('Cant find file: ', path_mutations)
-#' df_mutations <- read.csv(path_mutations, sep = "\t")
-#' df_maf <- df2maf(df_mutations, ref_genome = "hg38", col_sample = "sample_id")
 #'
 df2maf <- function(data,
                    ref_genome,
