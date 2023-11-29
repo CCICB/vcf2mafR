736064407f93eb0b116a06ead333ac56cbf79dcb
selkamand <sam.elkamand@gmail.com>
2023-11-29 23:30:51 +1100

    fix: removed no visible binding note

---
+++ R/globals.R
@@ -0,0 +1,3 @@
+utils::globalVariables(c(".data", "BIOTYPE", "Consequence", "SYMBOL", "biotype_priority",
+                         "cDNA_position", "csq_priority", "has_gene_symbol", "sample_type",
+                         "transcript_lengths"))
